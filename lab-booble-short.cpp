b02398415ca4bafe1ab4a30314f79f3ffbf71734
Fokrul <36418693+FokrulBhuiyan@users.noreply.github.com>
2019-08-09 18:25:52 +0600

    Add files via upload

---
+++ lab-booble-short.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int temp,n,arr[100],count;
+    cout<<"enter the element of numbers: ";
+    cin>>n;
+    cout<<"enter the elements: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n-1;i++){
+
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+
+            }
+        }
+
+    }
+    for(int j=0;j<n;j++){
+        cout<<"  "<<arr[j];
+    }
+}
